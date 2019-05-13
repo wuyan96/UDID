bfb0558b6616c4ee8ff9a64a3f75cc6dbbf00634
wuyan96 <844159670@qq.com>
2019-05-13 16:20:01 +0800

    no message

--- receive.php
+++ receive.php
@@ -3,5 +3,5 @@ $data = file_get_contents('php://input');
 //这里可以进行xml解析
 //header("Location: http://dev.skyfox.org/udid?data=".rawurlencode($data)); //有人说必须得目录形式才会安装成功
 header('HTTP/1.1 301 Moved Permanently');  //这里一定要301跳转,否则设备安装会提示"无效的描述文件"
-header("Location: http://test.tosneaker.com/index.php?".$params);
+header("Location: https://test.tosneaker.com/index.php?".$params);
 ?>
