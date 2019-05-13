cd8e61aed5b841e4b15e6e11189ea13b70627c81
wuyan96 <844159670@qq.com>
2019-05-13 16:33:55 +0800

    no message

--- receive.php
+++ receive.php
@@ -1,7 +1,86 @@
 <?php
 $data = file_get_contents('php://input');
-//这里可以进行xml解析
-//header("Location: http://dev.skyfox.org/udid?data=".rawurlencode($data)); //有人说必须得目录形式才会安装成功
-header('HTTP/1.1 301 Moved Permanently');  //这里一定要301跳转,否则设备安装会提示"无效的描述文件"
-header("Location: https://test.tosneaker.com/index.php?".$params);
+$plistBegin   = '<?xml version="1.0"';
+$plistEnd   = '</plist>';
+$pos1 = strpos($data, $plistBegin);
+$pos2 = strpos($data, $plistEnd);
+$data2 = substr ($data,$pos1,$pos2-$pos1);
+$xml = xml_parser_create();
+xml_parse_into_struct($xml, $data2, $vs);
+xml_parser_free($xml);
+
+$UDID = "";
+
+$CHALLENGE = "";
+
+$DEVICE_NAME = "";
+
+$DEVICE_PRODUCT = "";
+
+$DEVICE_VERSION = "";
+
+$iterator = 0;
+
+$arrayCleaned = array();
+foreach($vs as $v){
+    if($v['level'] == 3 && $v['type'] == 'complete'){
+
+    $arrayCleaned[]= $v;
+
+    }
+$iterator++;
+
+}
+
+$data = "";
+$iterator = 0;
+
+foreach($arrayCleaned as $elem){
+
+    $data .= "\n==".$elem['tag']." -> ".$elem['value']."<br/>";
+
+    switch ($elem['value']) {
+
+        case "CHALLENGE":
+
+            $CHALLENGE = $arrayCleaned[$iterator+1]['value'];
+
+            break;
+
+        case "DEVICE_NAME":
+
+            $DEVICE_NAME = $arrayCleaned[$iterator+1]['value'];
+
+            break;
+
+        case "PRODUCT":
+
+            $DEVICE_PRODUCT = $arrayCleaned[$iterator+1]['value'];
+
+            break;
+
+        case "UDID":
+
+            $UDID = $arrayCleaned[$iterator+1]['value'];
+
+            break;
+
+        case "VERSION":
+
+            $DEVICE_VERSION = $arrayCleaned[$iterator+1]['value'];
+
+            break;                       
+
+        }
+        $iterator++;
+
+}
+
+ 
+
+$params = "UDID=".$UDID."&CHALLENGE=".$CHALLENGE."&DEVICE_NAME=".$DEVICE_NAME."&DEVICE_PR ODUCT=".$DEVICE_PRODUCT."&DEVICE_VERSION=".$DEVICE_VERSION;
+
+//header("Location: http://dev.skyfox.org/udid?data=".rawurlencode($params));
+header('HTTP/1.1 301 Moved Permanently');
+header("Location: http://dev.skyfox.org/udid/index.php?".$params);
 ?>
